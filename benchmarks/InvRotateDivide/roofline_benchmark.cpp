d950696d99fcdced689103d9da484edd5308e808
tp5uiuc <tp5@illinois.edu>
2022-10-07 10:11:37 -0500

    feat : options in inv rotate divide benchmark

--- benchmarks/InvRotateDivide/roofline_benchmark.cpp
+++ benchmarks/InvRotateDivide/roofline_benchmark.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <string>
 
 // Add to include path
 #include "Vector3.h"
@@ -32,10 +33,35 @@ void kernel(Holder * const __restrict__ rod){
 	v_a_divide_b_equal_c(rod->tempVV3_nminus, rod->d0, rod->k0);
 }
 
+std::string getCmdOption(int argc, const char* argv[], const std::string& option)
+{
+    std::string cmd;
+     for( int i = 0; i < argc; ++i)
+     {
+          std::string arg = argv[i];
+          if(0 == arg.find(option))
+          {
+               std::size_t found = arg.find_first_of("=");
+               cmd =arg.substr(found + 1);
+               return cmd;
+          }
+     }
+     return cmd;
+}
 
-int main() {
-	const std::size_t n_values{1 << 16};  // 4096
-	const std::size_t n_samples{1 << 14};
+
+int main(const int argc, const char **argv) {
+	std::string n_values_as_str = getCmdOption(argc, argv, "-n_values=");
+	std::string n_samples_as_str = getCmdOption(argc, argv, "-n_samples=");
+	std::string angle_as_str = getCmdOption(argc, argv, "-inclination=");
+
+	const std::size_t n_values = std::stoul(n_values_as_str,nullptr,0);
+	const std::size_t n_samples = std::stoul(n_samples_as_str,nullptr,0);
+	const std::size_t increment = std::stod(angle_as_str, nullptr);
+
+	// const std::size_t n_values{1 << 16};  // 4096
+	// const std::size_t n_samples{1 << 14};
+
 	std::cout << "(" << n_values << ", " << n_samples << ")" << std::endl;
 
 	Holder holder(n_values);
@@ -49,7 +75,7 @@ int main() {
 	});
 	std::size_t n = 0UL;
 	std::generate(holder.Q.begin(), holder.Q.end(), [&]() mutable {
-		const auto angle = REAL(n) * M_PI / 20.0; // around 6.0
+		const auto angle = REAL(n) * increment;
 		const Vector3 r1{std::cos(angle), -std::sin(angle), 0.0};
 		const Vector3 r2{std::sin(angle), std::cos(angle), 0.0};
 		const Vector3 r3{0.0, 0.0, 1.0};
@@ -61,7 +87,7 @@ int main() {
 	for (std::size_t i = 0; i < n_samples; ++i)
 		kernel(&holder);
  
-#define CHECK_RESULTS
+// #define CHECK_RESULTS
 #ifdef CHECK_RESULTS
 	// Ensure no-nans for sanity
 	const bool no_nans = std::none_of(holder.k0.cbegin(), holder.k0.cend(), [](Vector3 const& v){
